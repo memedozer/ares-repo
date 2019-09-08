fe2cabecc48cda555af015d9376321d4142f5950
Huiritsu Hatesme <aseqw@tuta.io>
2019-09-08 04:48:26 +0300

    Some fixes.

--- install_tor.sh
+++ install_tor.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 {
-echo"Now , I going to install tor"
+echo "Now , I going to install tor"
+echo "But first, U need to edit ur USE flags maybe."
 }
 { 
 # some tor stuff 
-nano -w /etc/portage/make.conf 
+sudo nano -w /etc/portage/make.conf 
 
 
-#write use flags plz :D
+# write use flags plz :D
 
-
-emerge net-vpn/tor
-etc-update 
-emerge net-vpn/tor
+echo "Now, I will merge tor."
+sudo emerge net-vpn/tor
+echo "Some etc-update stuff."
+sudo etc-update
+sudo emerge net-vpn/tor
 
 }
 
@@ -20,14 +22,15 @@ emerge net-vpn/tor
 # start service immediately 
 # add service to start on system boot 
 # and add it to the default runlvl
-rc-service tor start
-rc-update add tor default
+echo "Editing runlevel for tor."
+sudo rc-service tor start
+sudo rc-update add tor default
 
 }
 
 
 # :D install tor version 8.5.4 
-
+echo "Wget stuff."
 wget https://www.torproject.org/dist/torbrowser/8.5.4/tor-browser-linux64-8.5.4_en-US.tar.xz
 tar xpvf tor-browser_US
 
@@ -35,4 +38,5 @@ tar xpvf tor-browser_US
 
 # that's all :D
 # Thank you for use my script
-# Script has maden by AreS
+# Script has maden by AreS,
+# modifed by veryhorny
